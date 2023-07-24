76c533093b7695b908788e46f89ceca317a2e9b7
Matvey <shul-m@mail.ru>
2023-07-24 13:14:42 +0300

    add validation

--- main.js
+++ main.js
@@ -3,16 +3,16 @@ import Swiper from 'swiper/bundle';
 import 'swiper/css/bundle';
 
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
+    //Optionalparameters
     direction: 'horizontal',
     loop: true,
 
-    // If we need pagination
+    //Ifweneedpagination
     pagination: {
         el: '.swiper-pagination',
     },
 
-    // Navigation arrows
+    //Navigationarrows
     navigation: {
         nextEl: '.swiper-button',
     },
@@ -21,6 +21,10 @@ const swiper = new Swiper('.swiper', {
 const menu = document.querySelector('.header__nav')
 const menuBurger = document.querySelector('.menu')
 const body = document.querySelector('body')
+const form = document.querySelector('.application__form')
+const inputName = document.querySelector('.application__input.name')
+const inputPhone = document.querySelector('.application__input.phone')
+const inputComment = document.querySelector('.application__comment')
 
 menuBurger.addEventListener('click', function () {
     menu.classList.toggle('active')
@@ -36,3 +40,49 @@ body.addEventListener('click', function (e) {
         }
     }
 })
+
+function validateForm() {
+    validateName()
+    validatePhone()
+    validateComment()
+    if (validateName() && validatePhone() && validateComment()) {
+        console.log('submit')
+    }
+}
+form.addEventListener('submit', function (e) {
+    e.preventDefault()
+    validateForm()
+})
+function validateName() {
+    if (inputName.value.match(/^([А-Я]{1}[а-яё]{1,23}|[A-Z]{1}[a-z]{1,23})$/)) {  // На одном языке и с большой буквы
+        inputName.classList.remove('wrong')
+        return true
+    } else {
+        inputName.classList.add('wrong')
+        return false
+    }
+}
+inputName.addEventListener('input', function () {
+    validateName()
+})
+function validatePhone() {
+    if (inputPhone.value.match(/^((8|\+7)[\-]?)?(\(?\d{3}\)?[\-]?)?[\d\-]{7,10}$/)) {
+        inputPhone.classList.remove('wrong')
+        return true
+    } else {
+        inputPhone.classList.add('wrong')
+        return false
+    }
+}
+inputPhone.addEventListener('input', function () {
+    validatePhone()
+})
+function validateComment() {
+    if (inputComment.value.match(/\w{5,}/)) {
+        inputComment.classList.remove('wrong')
+        return true
+    } else {
+        inputComment.classList.add('wrong')
+        return false
+    }
+}
